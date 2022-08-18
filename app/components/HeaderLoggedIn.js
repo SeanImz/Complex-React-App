338fc962add4dce41924fd9b5155a528bb793232
Sean <evans.seana@gmail.com>
2022-08-18 23:16:18 +0100

    Context and useReducer updated

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import ExampleContext from "../ExampleContext";
+import React, { useEffect, useContext } from "react"
+import { Link } from "react-router-dom"
+import DispatchContext from "../DispatchContext"
 
 function HeaderLoggedIn(props) {
-  const { setLoggedIn } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext)
 
   function handleLogout() {
-    setLoggedIn(false);
-    localStorage.removeItem("complexappToken");
-    localStorage.removeItem("complexappUsername");
-    localStorage.removeItem("complexappAvatar");
+    appDispatch({ type: "logout" })
+    localStorage.removeItem("complexappToken")
+    localStorage.removeItem("complexappUsername")
+    localStorage.removeItem("complexappAvatar")
   }
+
   return (
     <div className="flex-row my-3 my-md-0">
       <a href="#" className="text-white mr-2 header-search-icon">
@@ -21,10 +22,7 @@ function HeaderLoggedIn(props) {
         <span className="chat-count-badge text-white"> </span>
       </span>
       <a href="#" className="mr-2">
-        <img
-          className="small-header-avatar"
-          src={localStorage.getItem("complexappAvatar")}
-        />
+        <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} />
       </a>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
@@ -33,7 +31,7 @@ function HeaderLoggedIn(props) {
         Sign Out
       </button>
     </div>
-  );
+  )
 }
 
-export default HeaderLoggedIn;
+export default HeaderLoggedIn
