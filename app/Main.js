a922e3467875226f2f62df3e6cf84e731eea4841
Sean <evans.seana@gmail.com>
2022-08-20 16:38:45 +0100

    Added Immer, Profile page

--- app/Main.js
+++ app/Main.js
@@ -1,41 +1,64 @@
-import React, { useState, useReducer } from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Axios from "axios"
-Axios.defaults.baseURL = "http://localhost:8080"
+import React, { useState, useReducer, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { useImmerReducer } from "use-immer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
-import StateContext from "./StateContext"
-import DispatchContext from "./DispatchContext"
+import StateContext from "./StateContext";
+import DispatchContext from "./DispatchContext";
 
 // My Components
-import Header from "./components/Header"
-import HomeGuest from "./components/HomeGuest"
-import Home from "./components/Home"
-import Footer from "./components/Footer"
-import About from "./components/About"
-import Terms from "./components/Terms"
-import CreatePost from "./components/CreatePost"
-import ViewSinglePost from "./components/ViewSinglePost"
-import FlashMessages from "./components/FlashMessages"
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
 
 function Main() {
   const initialState = {
     loggedIn: Boolean(localStorage.getItem("complexappToken")),
-    flashMessages: []
-  }
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar"),
+    },
+  };
 
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        return { loggedIn: true, flashMessages: state.flashMessages }
+        draft.loggedIn = true;
+        draft.user = action.data;
+        return;
       case "logout":
-        return { loggedIn: false, flashMessages: state.flashMessages }
+        draft.loggedIn = false;
+        return;
       case "flashMessage":
-        return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+        draft.flashMessages.push(action, value);
+        return;
     }
   }
 
-  const [state, dispatch] = useReducer(ourReducer, initialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token);
+      localStorage.setItem("complexappUsername", state.user.username);
+      localStorage.setItem("complexappAvatar", state.user.avatar);
+    } else {
+      localStorage.removeItem("complexappToken");
+      localStorage.removeItem("complexappUsername");
+      localStorage.removeItem("complexappAvatar");
+    }
+  }, [state.loggedIn]);
 
   return (
     <StateContext.Provider value={state}>
@@ -44,7 +67,11 @@ function Main() {
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Routes>
-            <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
+            <Route path="/profile/:username/*" element={<Profile />} />
+            <Route
+              path="/"
+              element={state.loggedIn ? <Home /> : <HomeGuest />}
+            />
             <Route path="/post/:id" element={<ViewSinglePost />} />
             <Route path="/create-post" element={<CreatePost />} />
             <Route path="/about-us" element={<About />} />
@@ -54,12 +81,12 @@ function Main() {
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"))
-root.render(<Main />)
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
