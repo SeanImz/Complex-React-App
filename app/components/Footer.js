635e99aa2b82d6a88092d2eda6b63e28f095a33c
Sean <evans.seana@gmail.com>
2022-08-28 14:43:34 +0100

    greyhound app additions

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
       <p className="m-0">
         Copyright &copy; {new Date().getFullYear()}{" "}
         <a href="/" className="text-muted">
-          ComplexApp
+          GreyhoundApp
         </a>
         . All rights reserved.
       </p>
--- app/components/Home.js
+++ app/components/Home.js
@@ -63,10 +63,9 @@ function Home() {
           </h2>
           <p className="lead text-muted text-center">
             Your feed displays the latest posts from the people you follow. If
-            you don&rsquo;t have any friends to follow that&rsquo;s okay; you
-            can use the &ldquo;Search&rdquo; feature in the top menu bar to find
-            content written by people with similar interests and then follow
-            them.
+            you're not following anyone yet, that&rsquo;s okay; you can use the
+            &ldquo;Search&rdquo; feature in the top menu bar to find content
+            written by people with similar interests and then follow them.
           </p>
         </>
       )}
